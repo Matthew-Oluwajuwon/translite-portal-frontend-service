3887f5363e0a52eba423e901921722389a0f0364
sammydepoj <samueladepoju86@yahoo.com>
2023-09-15 18:13:52 +0100

    processor management upates

--- src/views/Processor/component/AddProcessor.tsx
+++ src/views/Processor/component/AddProcessor.tsx
@@ -1,19 +1,216 @@
 /* eslint-disable prettier/prettier */
 
 import { PageModal } from "@common/components/modal"
-import { useAppDispatch, useAppSelector } from "../../../store/hooks"
-import useToggle from "../../../custom-hooks/useToggle";
-
+import { useAppSelector } from "../../../store/hooks"
+import useToggle from "../../../custom-hooks/useToggle"
+import { Button, Col, Form, Input, Row, Select, Switch } from "antd"
+import useApiMethods, { FORM_ACTION } from "../../../custom-hooks/useApiMethods"
+import { apiEndpoints } from "../../../store/apiEndpoints"
+import useSetRequest from "../../../custom-hooks/useSetRequest"
 const AddProcessor: React.FC = () => {
-    const dispatch = useAppDispatch();
-    const state = useAppSelector(state => {
-        return state.global
-    });
-    
-    const { toggleFormModal } = useToggle()
+  const state = useAppSelector((state) => {
+    return state.global
+  })
+  const { setFormRequest } = useSetRequest()
+  const { toggleFormModal } = useToggle()
+  const { handleApiMethodController, result } = useApiMethods()
+  const getCardSchemes = () =>
+    handleApiMethodController(
+      state,
+      apiEndpoints.processor.cardSchemes,
+      "GET_BY_POST_METHOD",
+      {},
+      state.page,
+    )
   return (
-    <PageModal openModal={state.showFormModal} handleCancel={() => toggleFormModal(false)} centered modalFooter={false}>
-        {state.action === "CREATE" ? "New" : "Update"}
+    <PageModal
+      openModal={state.showFormModal}
+      modalWith="40rem"
+      handleCancel={() => toggleFormModal(false)}
+      centered
+      modalFooter={false}
+    >
+      {state.action === "CREATE" ? (
+        <div>
+          <h5 className="text-[#130F49] font-bold text-2xl ">
+            Setup New Processor
+          </h5>
+          <p className="text-base font-medium my-2 text-[#717E95]">
+            Add a new processor to the system
+          </p>
+        </div>
+      ) : (
+        <div>
+          <h5 className="text-[#130F49] font-bold text-2xl ">
+            Update Processor
+          </h5>
+          <p className="text-base font-medium my-2 text-[#717E95]">
+            Update an existing processor
+          </p>
+        </div>
+      )}
+      <Form
+        layout="vertical"
+        labelCol={{ span: 24 }}
+        wrapperCol={{ span: 24 }}
+        className="font-semibold"
+        onFinish={() =>
+          handleApiMethodController(
+            state,
+            state.action === "CREATE"
+              ? apiEndpoints.processor.addProcessor
+              : apiEndpoints.processor.updateProcessor + state.request?.id,
+            state.action as FORM_ACTION,
+            {
+              name: state.request?.name,
+              ip: state.request?.ip,
+              cardSchemes: state.request?.cardSchemes,
+              port: state.request?.port,
+              allowKeyExchange: state.request?.allowKeyExchange,
+            },
+            state.page,
+          )
+        }
+        fields={[
+          {
+            name: "name",
+            value: state.request?.name,
+          },
+          {
+            name: "ip",
+            value: state.request?.ip,
+          },
+          {
+            name: "port",
+            value: state.request?.port,
+          },
+          {
+            name: "allowKeyExchange",
+            value: state.request?.allowKeyExchange,
+          },
+          {
+            name: "cardSchemes",
+            value: state.request?.cardSchemes,
+          },
+        ]}
+      >
+        <Row style={{ width: "100%" }}>
+          <div className="w-full">
+            <Col span={24}>
+              <Form.Item
+                label={<h2 className="text-[#272848] mt-5">Processor Name</h2>}
+                name={"name"}
+              >
+                <Input
+                  className="py-5 px-5 mb-[-1rem]"
+                  onChange={(e) => setFormRequest(e.target.value, "name")}
+                  value={state.request?.name}
+                />
+              </Form.Item>
+            </Col>
+            <Col className="flex gap-4" span={24}>
+              <Form.Item
+                label={<h2 className="text-[#272848] mt-5">IP Address</h2>}
+                name={"ip"}
+                className="w-full"
+              >
+                <Input
+                  className="py-3 px-5 mb-[-1rem]"
+                  onChange={(e) => setFormRequest(e.target.value, "ip")}
+                  value={state.request?.ip}
+                />
+              </Form.Item>
+              <Form.Item
+                label={<h2 className="text-[#272848] mt-5">Port</h2>}
+                name={"port"}
+                className="w-full"
+              >
+                <Input
+                  className="py-3 px-5 mb-[-1rem]"
+                  onChange={(e) => setFormRequest(e.target.value, "port")}
+                  value={state.request?.port}
+                />
+              </Form.Item>
+            </Col>
+            <Col>
+              <Form.Item
+                label={
+                  <h2 className="text-[#272848] mt-5">
+                    Select Supported Card Schemes
+                  </h2>
+                }
+                name={"cardSchemes"}
+              >
+                <Select
+                  className="border rounded-lg py-3 w-full"
+                  mode="multiple"
+                  onFocus={getCardSchemes}
+                  onChange={(e) =>
+                    setFormRequest(
+                      e,
+                      "cardSchemes",
+                      result.data?.data?.cardSchemes,
+                      // "cardSchemes",
+                    )
+                  }
+                  value={state.request?.cardSchemes}
+                  loading={result.isLoading}
+                  options={
+                    Array.isArray(result.data?.data?.cardSchemes)
+                      ? (result.data?.data?.cardSchemes.map((x: any) => ({
+                          label: x,
+                          value: x,
+                        })) as any)
+                      : []
+                  }
+                  filterOption={(input: any, option: any) =>
+                    (option?.label ?? "")
+                      .toLowerCase()
+                      .includes(input.toLowerCase())
+                  }
+                />
+              </Form.Item>
+            </Col>
+            <Col className="flex gap-4" span={24}>
+              <Form.Item
+                label={
+                  <h2 className="text-[#272848] mt-5">Allow Key Exchange</h2>
+                }
+                name={"allowKeyExchange"}
+                className="w-full"
+              >
+                <Switch
+                  checked={state.request?.allowKeyExchange}
+                  onChange={(e) => setFormRequest(e, "allowKeyExchange")}
+                  style={{
+                    background: state.request?.allowKeyExchange
+                      ? "#38D79F"
+                      : "#ACAEC4",
+                  }}
+                />{" "}
+              </Form.Item>
+            </Col>
+            <Col span={24}>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="flex items-center justify-center bg-[#6D71F9] py-5 px-5 mx-auto"
+              >
+                Submit
+              </Button>
+            </Col>
+            <Col span={24} className="my-1">
+              <Button
+                type="text"
+                className="flex items-center justify-center py-5 px-5 mx-auto"
+                onClick={() => toggleFormModal(false)}
+              >
+                Cancel
+              </Button>
+            </Col>
+          </div>
+        </Row>
+      </Form>
     </PageModal>
   )
 }
